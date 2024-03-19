ff175f634ae9cc2e484e2484d8aea46c0f736a10
Ives Lima <tankianburaen@gmail.com>
2024-03-19 20:06:02 -0300

    Criação do Projeto

---
+++ src/main/java/com/senac/ProjetoFotos/ProjetoFotosApplication.java
@@ -0,0 +1,13 @@
+package com.senac.ProjetoFotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/ProjetoFotos/ProjetoFotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.ProjetoFotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
